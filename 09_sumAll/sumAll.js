90df985f3b625434750815b86d074177d828d1c7
Mohammad Aly <176561401+MuhammadAly11@users.noreply.github.com>
2025-11-26 22:21:50 +0200

    Solve foundations-loops-and-arrays exercises
    Solve 2nd part of foundations js exercises from 06 to 11

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function (start, end) {
+  let sum = 0;
+  if (
+    typeof start != "number" ||
+    typeof end != "number" ||
+    start < 0 ||
+    end < 0 ||
+    Math.floor(start) !== start ||
+    Math.floor(end) !== end
+  ) {
+    return "ERROR";
+  }
+  if (start > end) {
+    [start, end] = [end, start];
+  }
+  for (let i = start; i <= end; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+  let res = ((fahrenheit - 32) * 5) / 9;
+  res = parseFloat(res.toFixed(1));
+  return res;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  let res = (celsius * 9) / 5 + 32;
+  res = parseFloat(res.toFixed(1));
+  return res;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
